2c384f42db31e51804987b3aeee3a041a9041136
caipeichao <peichao.cai@fraudmetrix.cn>
2015-07-14 00:25:47 +0800

    最近共同祖先 解法2

---
+++ src/main/java/com/caipeichao/leetcode/Solution235_2.java
@@ -0,0 +1,27 @@
+package com.caipeichao.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by caipeichao on 15/7/13.
+ */
+public class Solution235_2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(true) {
+            if(p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if(p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else if(p.val < root.val && q.val > root.val) {
+                return root;
+            } else if(p.val > root.val && q.val < root.val) {
+                return root;
+            } else if(p.val == root.val || q.val == root.val) {
+                return root;
+            } else {
+                throw new RuntimeException("Impossible");
+            }
+        }
+    }
+}
