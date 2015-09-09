ad0f08d868dd1142c04037196514c75ccf87f462
caipeichao <peichao.cai@fraudmetrix.cn>
2015-09-09 22:14:44 +0800

    链表循环，不会

--- src/main/java/com/caipeichao/leetcode/Solution142.java
+++ src/main/java/com/caipeichao/leetcode/Solution142.java
@@ -1,12 +1,12 @@
 package com.caipeichao.leetcode;
 
 public class Solution142 {
-    public ListNode detectCycle(ListNode head) {
-//        if (head == null) return false;
-//
-//        ListNode node = head.next;
-//        while (true) {
-//            if(node == head) return true;
-//        }
-    }
+//    public ListNode detectCycle(ListNode head) {
+////        if (head == null) return false;
+////
+////        ListNode node = head.next;
+////        while (true) {
+////            if(node == head) return true;
+////        }
+//    }
 }
