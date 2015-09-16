b1b5135818332d60187dcc4c604cde473aa2f6a5
caipeichao <peichao.cai@fraudmetrix.cn>
2015-09-16 19:26:41 +0800

    完成所有子集，通过了测试

--- src/main/java/com/caipeichao/leetcode/Solution78.java
+++ src/main/java/com/caipeichao/leetcode/Solution78.java
@@ -1,64 +1,78 @@
 package com.caipeichao.leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Solution78 {
+    private int[] sortMap;
+
     public List<List<Integer>> subsets(int[] nums) {
-        if (nums.length == 0) return new ArrayList<List<Integer>>();
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for (int i = 0; i <= nums.length; i++) {
-            result.addAll(combination(nums, i));
-        }
-        return result;
-    }
+        // 构建排序映射
+        buildSortMap(nums);
 
-    public List<List<Integer>> combination(int[] nums, int k) {
-        if(k == 0) {
-            List<List<Integer>> result = new ArrayList<List<Integer>>();
-            result.add(new ArrayList<Integer>());
-            return result;
-        }
-
-        // 初始化循环变量
-        int n = nums.length;
-        int[] i = new int[k];
-        for (int j = 0; j < k; j++) {
-            i[j] = j;
-        }
-
-        // 每次增加一个，直到无法增加
+        boolean[] bits = new boolean[nums.length];
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         while (true) {
-            result.add(asList(nums, i));
-            if (!increase(i, n)) {
+            result.add(asList(nums, bits));
+            if (!increase(bits)) {
                 break;
             }
         }
         return result;
     }
 
-    private List<Integer> asList(int[] nums, int[] i) {
-        List<Integer> result = new ArrayList<Integer>();
-        for (int e : i) {
-            result.add(nums[e]);
-        }
-        return result;
-    }
-
-    public boolean increase(int[] i, int n) {
-        int k = i.length;
-        for (int j = k - 1; k >= 0; k--) {
-            i[j]++;
-            int max = n - (k - 1 - j);
-            if (i[j] < max) {
-                // 符合条件，准备退出
-                for (int m = j + 1; m < k; m++) {
-                    i[m] = i[m - 1] + 1;
+    private boolean increase(boolean[] bits) {
+        for (int i = 0; i < bits.length; i++) {
+            if (!bits[i]) {
+                bits[i] = true;
+                for (int j = 0; j < i; j++) {
+                    bits[j] = false;
                 }
                 return true;
             }
         }
         return false;
+    }
+
+    private List<Integer> asList(int[] nums, boolean[] bits) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < bits.length; i++) {
+            int j = sortMap[i];
+            if (bits[j]) {
+                result.add(nums[j]);
+            }
+        }
+        return result;
+    }
+
+    private void buildSortMap(int[] nums) {
+        List<Item> list = new ArrayList<Item>();
+        for (int i = 0; i < nums.length; i++) {
+            Item item = new Item();
+            item.num = nums[i];
+            item.index = i;
+            list.add(item);
+        }
+        Collections.sort(list, new ItemCompare());
+
+        sortMap = new int[nums.length];
+        for (int i = 0; i < list.size(); i++) {
+            sortMap[i] = list.get(i).index;
+        }
+    }
+
+    private static class Item {
+        public int num;
+        public int index;
+    }
+
+    private static class ItemCompare implements Comparator<Item> {
+
+        @Override
+        public int compare(Item o1, Item o2) {
+            return Integer.compare(o1.num, o2.num);
+        }
     }
 }
--- src/test/java/com/caipeichao/leetcode/Solution78Test.java
+++ src/test/java/com/caipeichao/leetcode/Solution78Test.java
@@ -1,5 +1,6 @@
 package com.caipeichao.leetcode;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -8,11 +9,20 @@ import static org.testng.Assert.*;
 
 public class Solution78Test {
 
-    @Test
-    public void testSubsets() throws Exception {
-        int[] a=TestUtils.parseIntArray("1,3,5,8,9");
+    @Test(dataProvider = "a")
+    public void testSubsets(String input, String expect) throws Exception {
+        int[] a=TestUtils.parseIntArray(input);
         List<List<Integer>> actual = new Solution78().subsets(a);
         String actual2 = TestUtils.toString(actual);
-        assertEquals(actual2, ",1,3,5,8,9,13,15,18,19,135,138,139,1358,1359,13589,");
+        assertEquals(actual2, expect);
+    }
+
+    @DataProvider(name = "a")
+    public Object[][] provide() {
+        return new Object[][]{
+                new Object[]{"1,2,3",",1,2,12,3,13,23,123,"},
+                new Object[]{"4,1,0",",4,1,14,0,04,01,014,"},
+                new Object[]{"1,3,5,7,8,9",",1,3,13,5,15,35,135,7,17,37,137,57,157,357,1357,8,18,38,138,58,158,358,1358,78,178,378,1378,578,1578,3578,13578,9,19,39,139,59,159,359,1359,79,179,379,1379,579,1579,3579,13579,89,189,389,1389,589,1589,3589,13589,789,1789,3789,13789,5789,15789,35789,135789,"},
+        };
     }
 }
