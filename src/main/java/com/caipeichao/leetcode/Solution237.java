a6cd2c7b7dbb78625bdfa1b381a810a18dfd2a6c
caipeichao <peichao.cai@fraudmetrix.cn>
2016-04-19 21:59:19 +0800

    237. Delete Node in a Linked List 完成

--- src/main/java/com/caipeichao/leetcode/Solution237.java
+++ src/main/java/com/caipeichao/leetcode/Solution237.java
@@ -1,6 +1,6 @@
 package com.caipeichao.leetcode;
 
-public class Solution238 {
+public class Solution237 {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
