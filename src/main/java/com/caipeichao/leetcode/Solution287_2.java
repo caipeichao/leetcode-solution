6f5d89c273ffc9c2d57482f7528ec319166c565a
caipeichao <peichao.cai@fraudmetrix.cn>
2016-04-22 22:23:39 +0800

    287. Find the Duplicate Number 完成方案二，有点理解了

--- src/main/java/com/caipeichao/leetcode/Solution287_2.java
+++ src/main/java/com/caipeichao/leetcode/Solution287_2.java
@@ -3,17 +3,12 @@ package com.caipeichao.leetcode;
 public class Solution287_2 {
     public int findDuplicate(int[] nums) {
         // find ring
-        int slow = nums.length;
+        int slow = nums.length; // 必须要从末尾开始走，不然可能第一个点就出现死循环
         int fast = nums.length;
-        System.out.println("slow: " + slow);
-        System.out.println("       fast: " + fast);
         while (true) {
             slow = nums[slow - 1];
-            System.out.println("slow: " + slow);
             fast = nums[fast - 1];
-            System.out.println("       fast: " + fast);
             fast = nums[fast - 1];
-            System.out.println("       fast: " + fast);
             if (slow == fast) break;
         }
 
@@ -21,9 +16,7 @@ public class Solution287_2 {
         int find = nums.length;
         while (true) {
             slow = nums[slow - 1];
-            System.out.println("slow: " + slow);
             find = nums[find - 1];
-            System.out.println("              find: " + find);
             if (slow == find) {
                 return slow;
             }
