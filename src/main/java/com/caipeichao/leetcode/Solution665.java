0816a5d8812ac423b8f54c9658858676f4e1d466
caipeichao <peichao.cai@fraudmetrix.cn>
2018-07-18 16:20:36 +0800

    665. Non-decreasing Array 修复答案错误

--- src/main/java/com/caipeichao/leetcode/Solution665.java
+++ src/main/java/com/caipeichao/leetcode/Solution665.java
@@ -3,12 +3,31 @@ package com.caipeichao.leetcode;
 public class Solution665 {
     public boolean checkPossibility(int[] nums) {
         if (nums.length <= 2) return true;
-        boolean modifyChance = true;
+        int point = findFirstDecreasingPoint(nums);
+        if (point < 0) return true;
+        if (replaceAndTestNonDecreasing(nums, point, nums[point + 1])) return true;
+        if (replaceAndTestNonDecreasing(nums, point + 1, nums[point])) return true;
+        return false;
+    }
+
+    private int findFirstDecreasingPoint(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] <= nums[i]) continue;
-            if (!modifyChance) return false;
-            modifyChance = false;
+            if (nums[i - 1] > nums[i]) {
+                return i - 1;
+            }
         }
-        return true;
+        return -1;
+    }
+
+    private boolean replaceAndTestNonDecreasing(int[] nums, int pos, int value) {
+        int backup = nums[pos];
+        nums[pos] = value;
+        boolean result = isNonDecreasing(nums);
+        nums[pos] = backup;
+        return result;
+    }
+
+    private boolean isNonDecreasing(int[] nums) {
+        return findFirstDecreasingPoint(nums) < 0;
     }
 }
